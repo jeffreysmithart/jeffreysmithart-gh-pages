a0eba882b7c3820fb7ea140ff6223b29e5a37067
Jeffrey <jeffreysmithart@gmail.com>
2015-07-25 20:55:01 -0500

    initial commit

---
+++ source/blog/2015-07-25-how-to-make-an-article.html.markdown
@@ -0,0 +1,8 @@
+---
+title: How to Make an Article
+date: 2015-07-25 18:51 UTC
+tags:
+---
+
+This is an article about how to make an article. You probably want to delete it and write your own articles!
+
---
+++ source/eclipse.php
@@ -0,0 +1,124 @@
+<?php 
+$pageTitle = "eclipse";
+include_once('inc/header.php');
+include_once('inc/nav.php'); ?>
+
+	<div class="eclipse content-wrapper">
+<?php include_once('inc/logo-bar.php'); ?>
+		<div class="header__wrapper">
+			<section id="header__box" class="portfolio__item__wrapper"> 
+				<article class="head__inner">
+					<img src="img/eclipse_logo.png" alt="">
+				</article>
+			</section>
+		</div>
+		<section id="top" class="portfolio__item">
+			<div class="portfolio__item__wrapper">
+				<article class="portfolio__item__text">
+					<p>Eclipse is the theme for the 2014 MCTC Apparel Technologies spring fashion show. The process began with a single word, eclipse. Print designer,  Rhia Brutger, and I were tasked with the challenge of building eclipse from a vague concept and general mood into a full campaign to support the spring fashion event.</p>
+				</article>
+			</div>
+		</section>
+		<section class="portfolio__item">
+			<div class="portfolio__item__wrapper">
+				<article class="portfolio__item__content">
+					<img src="img/eclipse_poster.jpg" alt="eclipse sketchbook | Jeffrey Smith">
+					
+				</article>
+				<article class="portfolio__item__content">
+					<p>The story really began by meeting with the Apparel Technologies students and faculty. They had a theme &mdash; eclipse &mdash; but they seemed unsure about what that actually meant. Our mission became helping the class get clear about their collective mission.</p> 
+					
+					
+				</article>
+
+			</div>
+						
+		</section>
+		<section class="portfolio__item">
+			<div class="portfolio__item__wrapper">
+				<blockquote>Eclipse: multiple bodies align to create a celestial event.
+Eleven designers with distinct perspectives converge to showcase their visions in a unique fashion event.</blockquote>
+				
+			</div>
+			
+		</section>
+
+		<section class="portfolio__item switch">
+			<div class="portfolio__item__wrapper">
+				<article class="portfolio__item__content">
+					<img src="img/eclipse_sketchbook.jpg" alt="eclipse sketchbook | Jeffrey Smith">
+					
+				</article>
+				<article class="portfolio__item__content">
+					<p>Once we had a direction to move towards, we began the process of designing. I started by researching &mdash; looking for inspiration through word definitions and imagery associated with the word eclipse. </p> 
+
+					<div class="eclipse__img__wrapper">
+						<img src="img/eclipse_logoplay.jpg" alt="">
+						<img src="img/eclipse_logoplay2.jpg" alt="">
+					</div>
+				</article>
+			</div>				
+		</section>
+
+		<section class="portfolio__item">
+			<div class="portfolio__item__wrapper">
+				
+				<article class="portfolio__item__img">
+					<img src="img/eclipse_logo_revision.png" alt="eclipse logo">
+				</article>
+			</div>
+		</section>
+
+		<section class="portfolio__item">
+			<h3>The Result</h3>
+			<hr>
+			<div class="portfolio__item__wrapper">
+				<article class="portfolio__item__content">
+					<img src="img/eclipse_ipad.png" alt="eclipse iPad | Jeffrey Smith" id="ipad">
+					
+				</article>
+
+				<div class="portfolio__item__content final">
+					<article class="portfolio__img">
+						<div class="img__outer__wrapper">
+					
+							<div class="browser">
+								<div class="indicators">
+									<span class="circle"></span>
+									<span class="circle"></span>
+									<span class="circle"></span>
+								</div>
+								<div class="img__url__bar"></div>
+							</div>
+							<div class="img__inner__wrapper">
+								<a href=""><img src="img/eclipse_home_full_wide.jpg" alt="Jeffrey Smith"></a>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+			<div class="portfolio__item__wrapper">
+				<blockquote>
+					<p>For the final version of the website, I built a custom, responsive grid using the Bourbon CSS framework and Neat &mdash; a lightweight semantic grid mixin library for Sass and Bourbon.</p>
+					<p>The designer's information has been organized into a flat-file structure. Designer pages are generated dynamically using PHP</p> 	
+				</blockquote>
+			</div>	
+				<img src="img/eclipse-Flat-Mockup.jpg" alt="eclipse | Jeffrey Smith" class="full-width">
+			
+			<div class="portfolio__item__wrapper">
+				<p class="portfolio__item__button"><a href="http://eclipse.mctcapparelportfolio.com/" >visit the site</a></p>
+			</div>
+						
+		</section>
+		<section class="next">
+			<a href="aura.php">
+				<div class="portfolio__item__wrapper">
+					<p>up next:</p>
+					<h2>Aura</h2>
+					<h4>2014 MCTC Photography Portfolio Show</h4>
+				</div>
+			</a>
+		</section>
+
+
+<?php include_once('inc/footer.php'); ?>
